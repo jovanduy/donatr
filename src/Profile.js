7d1e7ab23d21747904382e88f00cf23ec4cf7472
Jordan Van Duyne <jordannn808@gmail.com>
2017-11-09 16:23:09 -0500

    change textarea height to 16px

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ListGroup, ListGroupItem, Panel } from 'react-bootstrap';
 
+// Profile page component!
 class Profile extends Component {
 
     constructor() {
@@ -9,6 +10,8 @@ class Profile extends Component {
         this.toggleBenefits = this.toggleBenefits.bind(this);
         this.toggleHistory = this.toggleHistory.bind(this);
         this.toggleSettings = this.toggleSettings.bind(this);
+        // this state basically just holds the values for whether or 
+        // not each thing should be expanded
         this.state = {
             schedule: false,
             benefits: false,
@@ -17,6 +20,9 @@ class Profile extends Component {
         };
     }
 
+    // callback to toggle whether the schedule section is expanded
+    // by setting state.schedule to the opposite of whatever it
+    // previously was
     toggleSchedule() {
         this.setState((prevState, props) => {
             return {
@@ -83,7 +89,6 @@ class Profile extends Component {
                     <Panel collapsible expanded={this.state.settings}>
                         You've donated a lot
                     </Panel>
-
                     <ListGroupItem bsStyle="danger" onClick={this.props.logout}>Logout</ListGroupItem>
                 </ListGroup>
             </div>
