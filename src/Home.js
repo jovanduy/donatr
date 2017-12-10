a4522ebf2cdf8282dd7117ef2ffa50c6a3b844b7
Jordan Van Duyne <jordannn808@gmail.com>
2017-12-10 16:34:28 -0500

    update homepage text

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,14 @@ class Home extends Component {
                 <p className=" description about">
                     Can’t take your donations to a donation center?
                 </p>
+                <p className="description">
+                    We'll help you make a difference!
+                </p>
+                <p className="description">
+                    DONATR connects donors like you to volunteers who pickup your donations and deliver them to local organizations. <span className="learn-more">Learn more about us.</span>
+                </p>
                 <p className="description get-started">
                     What do you want to donate? Where? 
-                </p>
-
-                <p className="description get-started">
-DONATR connects donors like you to volunteers who deliver donations to local organizations.
                 </p>
             </div>
         );
